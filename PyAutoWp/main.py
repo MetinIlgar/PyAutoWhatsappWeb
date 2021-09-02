ae56de2994dd2eab2f1144ab40cd7a0c373b17b1
Metin Ilgar <ilgarmutlu@outlook.com.tr>
2021-09-03 01:13:53 +0300

    Big Update
    Added new timer() function.
    Updated differentCountryTimer() function.
    Updated vcfReader() function.
    contacts_df_edit() was created to edit the contact list.

--- PyAutoWp/main.py
+++ PyAutoWp/main.py
@@ -6,6 +6,7 @@ import pytz
 from pytz import timezone
 from time import sleep
 from datetime import datetime
+import phonenumbers
 
 def windowCheck():
 	web.open("https://web.whatsapp.com/")
@@ -29,18 +30,28 @@ def windowClose():
 
 def differentCountryTimer():
 	t = input("Enter the date and time to send the message (example: day.month.year 21:00): ")
+	try : 
+		phoneNumber = phonenumbers.parse(phoneNumber)
+		time_Zone = phonenumbers.timezone.time_zones_for_number(phoneNumber) # return tuple
+		live_in = input(f"Does the owner of the phone number live in {time_Zone}? (Y,N)")
+		if live_in == "y" or live_in == "Y":
+			return str(time_Zone[0])
 
-	tz = input("Country Abbreviations (example: tr, us etc.): ")
-	tz_list = pytz.country_timezones[tz]
-	num = 1
-	tz_dic = {}
-	for i in tz_list:
-		tz_dic[num] = i
-		print(str(num) + " - " + str(tz_dic[num]))
-		num = num+1
+	except:
+		tz = input("Country Abbreviations (example: tr, us etc.): ")
+		tz_list = pytz.country_timezones[tz]
+		num = 1
+		tz_dic = {}
+		for i in tz_list:
+			tz_dic[num] = i
+			print(str(num) + " - " + str(tz_dic[num]))
+			num = num+1
 
-	tz_val = int(input("Please enter the location number to which you will send the message (example: 1, 2, 3 etc.): "))
+		tz_val = int(input("Please enter the location number to which you will send the message (example: 1, 2, 3 etc.): "))
 
+		return tz_dic[tz_val]
+
+def timer(tz):
 	format = "%d-%m-%Y %H:%M"
 	 
 	while True:
@@ -48,12 +59,12 @@ def differentCountryTimer():
 		now_utc = datetime.now(timezone('UTC'))
 
 		# Convert to x time zone
-		now_x = (now_utc.astimezone(timezone(tz_dic[tz_val]))).strftime(format)
+		now_x = (now_utc.astimezone(timezone(tz))).strftime(format)
 
 		later_x = datetime.strptime(t,"%d.%m.%Y %H:%M").strftime(format)
 
 		if now_x == later_x:
-			return True
+			return True		
 
 def sendMessage(phoneNumber, message, internationalTelephoneCodes = "90"):
 	windowOpen(f"https://web.whatsapp.com/send?phone={internationalTelephoneCodes}{phoneNumber}&text={message}")
@@ -104,11 +115,13 @@ def vcfReader(path, fileName):
 					key = "Phone Number"
 
 					val = val.replace("\n", "")
-					val = val.replace("+", "")
 					val = val.replace("-", "")
 
 					contacts[numPerson][key] = val
-					contacts[numPerson]["Country Phone Codes"] = ""
+
+					if "+" in val:
+						contacts[numPerson]["Country Phone Codes"] = str(phonenumbers.parse(val).country_code)
+
 					break
 
 				elif i == "":
@@ -117,3 +130,47 @@ def vcfReader(path, fileName):
 
 					contacts_df.to_excel(fileName, index = False)
 					return contacts_df
+
+def contacts_df_edit(contacts_df):
+	print(contacts_df)
+	k=input("Is there a phone number you want to remove from the list? (Y/N): ")
+	print("_______________________________________________________________________")
+	if k == "Y" or k == "y":
+		delete_num=input("Write the sequence numbers of the phone numbers you want to remove from the list, separating them with commas (eg 1,2,3):")
+		print("_______________________________________________________________________")
+		delete_list=delete_num.split(",")
+		for m in delete_list:
+			print(contacts_df.loc[int(m)]["Name"]," has been deleted.")
+			contacts_df.drop(int(m), axis = 0, inplace = True)
+		print("_______________________________________________________________________")
+
+	if contacts_df.isnull().values.any() == True:
+		NANdata= contacts_df.isnull().sum()
+		if NANdata["Name"] != 0:
+			print(f"There are {str(NANdata['Name'])}  missing names in the list of names.")
+
+		if NANdata["Phone Number"] != 0:
+			print(f"There are {str(NANdata['Phone Number'])} missing numbers in the phone list.")
+
+		if NANdata["Country Phone Codes"] != 0:
+			print(f"There are {NANdata['Country Phone Codes']} missing country phone codes in the country phone codes list.")
+			print("_______________________________________________________________________")
+			x=input("Enter 1 if you want to assign collectively, \nenter 2 if you want to assign one by one, \nand 0 if you do not want to assign. \n(If you do not assign, your message will not be forwarded to those people.): ")
+			print("_______________________________________________________________________")
+			if x == "1":
+				cc = input("Enter the country code you want to bulk assign. (eg 90.):")
+				contacts_df['Country Phone Codes'].fillna(cc, inplace = True)
+				print("_______________________________________________________________________")
+
+			if x == "2":
+				isnullList=contacts_df['Country Phone Codes'].isnull()
+				index_list = (isnullList[isnullList==True].index)
+				for i in index_list:
+					print(contacts_df.loc[i]["Name"]+": "+contacts_df.loc[i]["Phone Number"])
+					n = input("Country Phone Codes for this contact: ")
+					contacts_df.loc[i]['Country Phone Codes'] = n
+					print("-------------------------------")
+			if x == "0":
+				contacts_df.dropna(inplace = True)
+
+	return contacts_df		
